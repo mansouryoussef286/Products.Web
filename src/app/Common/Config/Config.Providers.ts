d1f91861d98b264d7569ae27bc617d1a53876201
Youssef Mansour <mansouryoussef286@gmail.com>
2024-08-22 14:07:45 +0300

    deploy fixes

--- src/app/Common/Config/Config.Providers.ts
+++ src/app/Common/Config/Config.Providers.ts
@@ -1,5 +1,5 @@
 import { APP_INITIALIZER, ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withHashLocation } from '@angular/router';
 
 import { routes } from '../../Base/app.routes';
 import {
@@ -13,7 +13,7 @@ import { AuthInterceptor } from '../Interceptors/Auth.Interceptor';
 
 export const appConfigProviders: ApplicationConfig = {
   providers: [
-    provideRouter(routes),
+    provideRouter(routes, withHashLocation()),
     provideHttpClient(withInterceptorsFromDi()),
     {
       provide: APP_INITIALIZER,
